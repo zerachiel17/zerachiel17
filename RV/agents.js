4f53bf5c0fb8efc987bdaca7fa4dcaf1958d2582
zerachiel17 <goye256@gmail.com>
2016-05-29 20:52:25 -0500

    Create agents.js

---
+++ RV/agents.js
@@ -0,0 +1,40 @@
+function Agent(x=0, y=0) {
+  THREE.Object3D.call(this);
+  this.position.x = x;
+  this.position.y = y;
+}
+
+Agent.prototype =  new THREE.Object3D();
+
+Agent.prototype.sense = function(environment) {};
+Agent.prototype.plan = function(environment) {};
+Agent.prototype.act = function(environment) {};
+
+function Environment() {
+  THREE.Scene.call(this);
+}
+
+//Para nuestro caso, el prototipo de un entorno es Scene()
+Environment.prototype = new THREE.Scene();
+
+//La interfaz del entorno y los agentes estara dada por los siguientes metodos
+Environment.prototype.sense = function() {
+  for ( var i = 0; i < this.children.length; i++ ) {
+    if (this.children[i].sense !== undefined)
+            this.children[i].sense(this);
+  }
+}
+
+Environment.prototype.plan = function() {
+  for ( var i = 0; i < this.children.length; i++ ) {
+    if (this.children[i].plan !== undefined)
+            this.children[i].plan(this);
+  }
+}
+
+Environment.prototype.act = function() {
+  for ( var i = 0; i < this.children.length; i++ ) {
+    if (this.children[i].act !== undefined)
+            this.children[i].act(this);
+  }
+}
